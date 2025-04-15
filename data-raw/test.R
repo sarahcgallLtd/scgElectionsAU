f7c702fbc9fdad3727fea84daaeffefb7ef9373e
Sarah C Gall <contact@sarahcgall.co.uk>
2025-04-15 09:54:48 +0100

    fixing pkgdown error

--- data-raw/test.R
+++ data-raw/test.R
@@ -4,3 +4,12 @@ df <- get_aec_data(
   category = "Senate"
 )
 
+national_list_of_candidates <- suppressMessages(
+  get_aec_data(file_name = "National list of candidates",
+  date_range = list(from = "2024-04-13", to = "2024-06-13"),
+  type = c("By-Election")
+)
+
+
+
+rmarkdown::render("C:/Users/SarahGall/Programming/PyCharm/Projects/Packages/scgElectionsAU/vignettes/articles/aec-raw-data.rmd")
